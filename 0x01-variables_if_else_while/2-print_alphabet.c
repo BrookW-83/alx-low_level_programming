dfb07f2b66a2d1fac375db71dee458ab17bb5609
Brook <brookwworku3@gmail.com>
2022-10-23 12:25:10 -0700

    Task 2,2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char l;
+
 	l = 'a';
 
 	while (l <= 'z')
