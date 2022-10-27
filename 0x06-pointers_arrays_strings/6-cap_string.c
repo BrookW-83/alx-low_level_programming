696fbcf37965a65c9e2d6005865583dd9b2f5e2f
Brook <brookwworku3@gmail.com>
2022-10-27 12:21:21 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizing all the words in a string
+ * @n: string to capitalize
+ * Return: n
+ */
+
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[++i])
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+			i++;
+
+		if (n[i - 1] == ' ' ||
+				n[i - 1] == '\t' ||
+				n[i - 1] == '\n' ||
+				n[i - 1] == ',' ||
+				n[i - 1] == ';' ||
+				n[i - 1] == '.' ||
+				n[i - 1] == '!' ||
+				n[i - 1] == '?' ||
+				n[i - 1] == '"' ||
+				n[i - 1] == '(' ||
+				n[i - 1] == ')' ||
+				n[i - 1] == '{' ||
+				n[i - 1] == '}')
+			str[i] -= 32;
+	}
+	return (n);
+}
