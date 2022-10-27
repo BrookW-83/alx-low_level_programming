7572da3cf84dd40afbebb91775bfebf5f77d0c1f
Brook <brookwworku3@gmail.com>
2022-10-27 11:44:59 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array
+ * @a: input array
+ * @n: number of elements
+ * Return: None
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, i;
+
+	for (i = n - 1; i > n / 2; i--)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = tmp;
+	}
+}
