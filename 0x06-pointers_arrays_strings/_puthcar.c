319fc43ef496cef43968e8a57841e4dd5e961cd2
Brook <brookwworku3@gmail.com>
2022-10-27 07:46:36 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/_puthcar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes character i to stdout
+ * @i: char to printed
+ * Return: 1 (Success)
+ */
+
+int _putchar(char i)
+{
+	return (write(1. &i, 1));
+}
