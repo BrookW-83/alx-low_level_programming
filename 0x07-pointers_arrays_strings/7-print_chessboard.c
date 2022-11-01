40dbd3f89ac72999a4370ebc019bba030b4ecffb
Brook <brookwworku3@gmail.com>
2022-11-01 12:26:01 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -16,10 +16,10 @@ void print_chessboard(char (*a)[8])
 		m = 0;
 		while (m < 8)
 		{
-			_putchar(a[n][m]);
+			_puthcar(a[n][m]);
 
 			if (m == 7)
-				_putchar('\n');
+				_puthcar('\n');
 			m++;
 
 		}
