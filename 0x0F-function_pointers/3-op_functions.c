341d5ffe2e1ef4a0fd16bc6cef0ef1d7c69f5d29
Brook <brookwworku3@gmail.com>
2022-11-18 13:26:57 -0800

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculates sum of two ints
+ * @i: first input
+ * @j: second input
+ * Return: Sum
+ */
+
+int op_add(int i, int j)
+{
+	return (i + j);
+}
+
+/**
+ * op_sub - calculates diffrence of two values
+ * @i: first input
+ * @j: second input
+ * Return: diff
+ */
+
+int op_sub(int i, int j)
+{
+	return (i - j);
+}
+
+/**
+ * op_mul - calculates multiplication of two values
+ * @i: first input
+ * @j: second input
+ * Return: product
+ */
+
+int op_mul(int i, int j)
+{
+	return (i * j);
+}
+
+/**
+ * op_div - calculates divsion of two values
+ * @i: first inpui
+ * @j: second input
+ * Return: Division
+ */
+
+int op_div(int i, int j)
+{
+	return (i / j);
+}
+
+/**
+ * op_mod - calculates modualr division of two values
+ * @i: first input
+ * @j: second input
+ * Return: mod
+ */
+
+int op_mod(int i, int j)
+{
+	return (i % j);
+}
